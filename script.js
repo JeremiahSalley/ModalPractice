2fcd4c396289abd004f9a15a23b0fb7a8cbfd9c2
Jeremiah Salley <Jeremiah.salley@yahoo.com>
2021-04-05 12:53:47 -0400

    done

---
+++ script.js
@@ -0,0 +1,11 @@
+const open = document.getElementById("open")
+const close = document.getElementById('close')
+const modalContainer = document.getElementById('modalContainer')
+
+open.addEventListener('click', () =>{
+    modalContainer.classList.add('show')
+})
+
+close.addEventListener('click', () =>{
+    modalContainer.classList.remove('show')
+})
